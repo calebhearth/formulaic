d59b0eb3ad4cfcd4e280fccc7b2c07c45b73be35
Caleb Thompson <caleb@calebthompson.io>
2013-07-11 15:16:23 -0400

    Add submit helper

--- lib/formulaic/dsl.rb
+++ lib/formulaic/dsl.rb
@@ -7,5 +7,9 @@ module Formulaic
     def input(model_name, field, action = :create)
       Label.new(model_name, field, action).to_str
     end
+
+    def submit(model_class, action = :create)
+      I18n.t([:helpers, :submit, model_class, action].join('.'))
+    end
   end
 end
