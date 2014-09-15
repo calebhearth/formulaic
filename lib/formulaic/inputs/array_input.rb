8e882d14e8011c7169afd9d04569a145cf29818c
Sean Doyle <sean.p.doyle24@gmail.com>
2014-09-15 17:27:07 -0400

    Rescue `Capybara` errors and rethrow as Formulaic

--- lib/formulaic/inputs/array_input.rb
+++ lib/formulaic/inputs/array_input.rb
@@ -2,11 +2,27 @@ module Formulaic
   module Inputs
     class ArrayInput < Input
       def fill
-        if page.has_select?(label)
-          value.each { |option| select option, from: label }
+        if has_select?
+          select_options
         else
-          value.each { |checkbox| check checkbox }
+          check_boxes
         end
+      rescue Capybara::ElementNotFound
+        raise InputNotFound.new(%[Unable to find input "#{label}".])
+      end
+
+      private
+
+      def has_select?
+        page.has_select?(label)
+      end
+
+      def select_options
+        value.each { |option| select option, from: label }
+      end
+
+      def check_boxes
+        value.each { |checkbox| check checkbox }
       end
     end
   end
