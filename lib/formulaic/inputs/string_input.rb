25ccccb3592142ffe9d0995be3837c7e9ea6da3a
Adrian Rangel <adrian.rangel@gmail.com>
2015-02-07 04:03:27 -0600

    add configurable default_wait_time

--- lib/formulaic/inputs/string_input.rb
+++ lib/formulaic/inputs/string_input.rb
@@ -2,9 +2,9 @@ module Formulaic
   module Inputs
     class StringInput < Input
       def fill
-        if page.has_selector?(:fillable_field, label)
+        if page.has_selector?(:fillable_field, label, wait: Formulaic.default_wait_time)
           fill_in(label, with: value)
-        elsif page.has_selector?(:radio_button, label)
+        elsif page.has_selector?(:radio_button, label, wait: Formulaic.default_wait_time)
           choose(value)
         elsif has_option_in_select?(value, label)
           select(value, from: label)
@@ -14,7 +14,7 @@ module Formulaic
       end
 
       def has_option_in_select?(option, select)
-        find(:select, select).has_selector?(:option, option)
+        find(:select, select).has_selector?(:option, option, wait: Formulaic.default_wait_time)
       rescue Capybara::ElementNotFound
         false
       end
