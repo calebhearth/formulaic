5b3d2a56818698b3fa2f921268fff0c635ac17f3
Hannah Oppenheimer <hana378@gmail.com>
2015-12-24 16:51:36 -0500

    reduce line character length

--- spec/formulaic/form_spec.rb
+++ spec/formulaic/form_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe Formulaic::Form do
   context "attribute is invalid type" do
     it "throws an error" do
-      expect { Formulaic::Form.new(nil, nil, { invalid: nil }) }.to raise_error(Formulaic::Form::InvalidAttributeTypeError)
+      invalid_attribute_error = Formulaic::Form::InvalidAttributeTypeError
+
+      expect{ formulaic_request }.to raise_error(invalid_attribute_error)
+    end
+
+    def formulaic_request
+      Formulaic::Form.new(nil, nil, { invalid: nil })
     end
   end
 end
